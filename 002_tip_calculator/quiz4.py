e02505a0079c3aab7d4c9707cb51007838851864
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-04 15:05:49 -0300

    Update tip calculator code

--- 002_tip_calculator/quiz4.py
+++ 002_tip_calculator/quiz4.py
@@ -11,3 +11,5 @@ weeks = (90 - (int(age_int))) * 52
 months = (90 - int(age_int)) * 12
 
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+a = int('5') / int(2.7)
+print(type(a))
