f68452e1b125733f03e94806cd1123a21c669686
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-07 12:22:46 -0300

    Add notes on python functions

---
+++ 006_escaping_the_maze/functions.py
@@ -0,0 +1,15 @@
+# Name of function followed by ().
+num_char = len("Hello")
+print(num_char)
+
+# Writing your own function.
+
+
+def my_function():  # Everything after ":" is indented as part of function.
+    print("Hello!")
+    print("Bye!")
+
+# You must call the function and, if needed, an input.
+
+
+my_function()
