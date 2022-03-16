78e1eab725b1f289e8d9adb46edf58f98a1c4073
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-15 22:21:55 -0300

    Fix error in blackjack  game code

--- 011_blackjack/blackjack.py
+++ 011_blackjack/blackjack.py
@@ -93,7 +93,7 @@ def play_game():
     while computer_score != 0 and computer_score < 17:
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
-        
+
     print(f"    Your final hand: {user_cards}, final score: {user_score}")
     print(f"    Computer's final hand: {computer_cards},"
           " final score: {computer_score}")
