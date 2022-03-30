ec5071f86b00959b4cc74ec54b208a0f661c2823
Felipe Sebben <felipesebben@yahoo.com.br>
2022-03-30 18:17:12 -0300

    Update US State game code by applying list comprehension concepts

--- 025_CSV_DA_pandas/us_states_game/main.py
+++ 025_CSV_DA_pandas/us_states_game/main.py
@@ -17,10 +17,7 @@ while len(guess_states) < 50:
     print(answer_state)
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guess_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guess_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
