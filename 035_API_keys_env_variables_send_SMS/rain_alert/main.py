52f1071be781575dbea9e2b00158f73d754c4b25
Felipe Sebben <felipesebben@yahoo.com.br>
2022-05-03 22:11:30 -0300

    Update main.py with final version

--- 035_API_keys_env_variables_send_SMS/rain_alert/main.py
+++ 035_API_keys_env_variables_send_SMS/rain_alert/main.py
@@ -1,7 +1,9 @@
 import requests
+from twilio.rest import Client
+
 
 OWM_Endpoint = 'https://api.openweathermap.org/data/2.5/onecall?'
-api_key = {API_CODE_HERE}
+api_key = {OWM_API_KEY}
 
 latitude = -30.034647
 longitude = -51.217659
@@ -16,11 +18,18 @@ parameters = {
 response = requests.get(url=OWM_Endpoint, params=parameters)
 response.raise_for_status()
 
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = {YOUR_ACCOUNT_SID}
+auth_token = {YOUR_AUTH_TOKEN}
+
+
 # Print status code to check whether our request went by.
-print(response.status_code)
+# print(response.status_code)
 
 weather_data = response.json()
 hourly_data = list(weather_data.values())
+
 # Get hold of the first 12 hours of the  response by slicing through the json file.
 weather_slice = weather_data['hourly'][:12]
 # print(weather_data['hourly'][0]['weather'][0]['id'])
@@ -34,4 +43,11 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("Nênee, vai chovê aí, pega um guarda-chuva! 🌂 .")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="Nenee, vai chovê aí, pega um 🌂!.",
+        from_={API_NR},
+        to={CALLER_NR}
+    )
+    print(message.status)
